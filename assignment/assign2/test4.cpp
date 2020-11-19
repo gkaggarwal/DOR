4fba3ee8c507bdeb09a1bca6c64155bae4013994
gkaggarwal <gkaggarwal266@gmail.com>
2020-11-19 21:57:56 +0530

    commit on 19 nov

--- assignment/assign2/test4.cpp
+++ assignment/assign2/test4.cpp
@@ -15,7 +15,7 @@ struct Hashnode
   long int *posting;
 };
 struct Hashnode* HashMap[TABLE_SIZE];
-long int Array[TABLE_SIZE][2000]={0};
+//long int Array[TABLE_SIZE][2000]={0};
 
 static long int counter_insert; //counter for insertion
 static long int counter_delete;
@@ -34,6 +34,9 @@ void Insert(long int term_id,long int freq, long int doc_id,int check=0,Hashnode
    struct Hashnode* item=(struct Hashnode*)malloc(sizeof(struct Hashnode*));
    item->frequency=freq;
    item->term_id=term_id;
+   
+   (item->posting)=(long int*)calloc(8000,sizeof(int));
+   //*(item->posting)=0;
    long int h_index=HashIndex(term_id);
    
    while(HashMap[h_index]!=NULL && HashMap[h_index]->term_id!=-1)
@@ -48,8 +51,9 @@ void Insert(long int term_id,long int freq, long int doc_id,int check=0,Hashnode
    if(counter==TABLE_SIZE)
    cout<<"No space available to insert\n";
    else
-   { item->posting=Array[h_index];
-     Array[h_index][0]=doc_id;
+   { //item->posting=Array[h_index];
+     //Array[h_index][0]=doc_id;
+     *(item->posting)=doc_id;
      HashMap[h_index]=item;
      counter_insert++;
    }
@@ -58,11 +62,11 @@ void Insert(long int term_id,long int freq, long int doc_id,int check=0,Hashnode
    {
    item1->frequency+=freq;
    long int i=0;
-   while(i<2000 && *(item1->posting+i)!=0)
+   while(i<8000 && *(item1->posting+i)!=0)
    {
     i++;
    }
-   if(i<2000)
+   if(i<8000)
    {
    (*(item1->posting+i))=doc_id;
    }
@@ -97,7 +101,7 @@ void display_posting(Hashnode* item1)
  long int *p;
  p=item1->posting;
  int i=0;
- while( i<2000 && *(p+i)!=0 )
+ while( i<8000 && *(p+i)!=0 )
  {
  cout<<*(p+i)<<",";
  i++;
@@ -112,6 +116,33 @@ int main()
     string word1_doc,word1_string,word1_freq,word2,word3, filename1,filename2; 
     filename1 = "dict0.dat"; 
     filename2 = "vocab.dat";
+    //-----------------------------------------------------------------
+    string s ;
+    string A[100];
+    char s1[100];
+    cout<<"Enter Query:";
+    fgets(s1,100,stdin);
+    s=s1;
+    std::string delimiter = " AND ";
+    int k=1;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+    token = s.substr(0, pos);
+    
+    A[k++]=token;
+    
+    s.erase(0, pos + delimiter.length());
+   }
+   pos = s.find("\n");  // Deleteing newline character at the end of input string
+   s.erase(pos,pos+1);
+    A[k]=s;
+    for(int i=1;i<=k;i++)
+    {
+    cout<<A[i]<<i<<endl;
+    }
+    cout<<"k="<<k<<endl;
+    //---------------------------------------------------------------------
     file1.open(filename1.c_str(),ios::in);  
     while( file1 >> word1_doc)
     {  long int term_id=0;
@@ -157,18 +188,21 @@ int main()
       
     }
     }
- while(1)
- {   int switch_case;
-     cout<<"press 1 for search"<<endl;
-     cout<<"press 2 to exit"<<endl;
-     cin>>switch_case;
+    int p=1;
+    
+ while(k)
+ {   int switch_case=1;
+     //cout<<"press 1 for search"<<endl;
+     //cout<<"press 2 to exit"<<endl;
+     //cin>>switch_case;
      if(switch_case==1)
      {
        
-         cout<<"Enter word for search:";
+       //  cout<<"Enter word for search:";
          string search_word;
          long int search_term_id=0;
-         cin>>search_word;
+         //cin>>search_word;
+         search_word= A[p];
          file2.open(filename2.c_str(),ios::in);
        while ( file2 >> word2)
        {
@@ -186,8 +220,11 @@ int main()
        
        }
        Hashnode* item2=Search(search_term_id);
+       cout<<"posting list of "<<A[p]<<endl;
+       p++;
+       k--;
        display_posting(item2);
-     cout<<"search term id : "<<search_term_id<<endl;
+      cout<<"search term id : "<<search_term_id<<endl;
        
      }
     else
@@ -196,6 +233,8 @@ int main()
        free(HashMap[TABLE_SIZE]);
        }
 }
+free(HashMap[TABLE_SIZE]);
+       
 return 0;
 }
 
